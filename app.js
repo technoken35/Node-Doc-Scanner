54aac55da10016be6062b360ba7684e6139f9d99
Kendall Hayes <hayeskendall21@gmail.com>
2020-10-25 12:39:55 -0700

    scanner route now returns json with google OCR data

--- app.js
+++ app.js
@@ -3,39 +3,38 @@ const express = require('express');
 const app = express();
 const ipAddress = '127.0.0.1';
 const port = 8124;
+
+// Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
+// Creates a client
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: './gcp/APIKey.json',
+});
 
 async function runOCR() {
-  // Imports the Google Cloud client library
-
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient({
-    keyFilename: './gcp/APIKey.json',
-  });
-
   // Performs label detection on the image file
   const [result] = await client.labelDetection('./resources/ball.jpg');
-  const [textResult] = await client.textDetection('./resources/pdf-files.jpg');
+  [textResult] = await client.textDetection('./resources/pdf-files.jpg');
   const text = textResult.textAnnotations;
   const labels = result.labelAnnotations;
-  //console.log('Labels:');
-  //labels.forEach((label) => console.log(label.description));
-  //console.log('Text Detection:');
-  // text.forEach((item) => console.log(item.description));
 
   const data = [text, labels];
-  console.log(data[0]);
-  return data;
+  console.log(data[0][0]);
+  // pass quickbooks data
+  //callback(data);
+  //return data;
 }
+
+runOCR();
 
 app.get('/', (req, res) => {
   res.send('homepage route');
 });
 
-app.get('/scanner', (req, res) => {
-  res.send('Scanner route');
-  data = runOCR();
-  console.log(typeof data, data, 'data in scanner route');
+app.get('/scanner', async (req, res) => {
+  const [asyncData] = await client.labelDetection('./resources/nodejs.jpg');
+  const data = asyncData.labelAnnotations;
+  res.send(data);
 });
 
 app.listen(port, ipAddress, () => {
